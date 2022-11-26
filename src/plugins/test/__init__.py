888881a9ec8ff03e150e32b14eaa18e7453194ed
yzyyz1387 <1796031384@qq.com>
2022-11-26 13:41:17 +0800

    :card_file_box: 插件目录

---
+++ src/plugins/test/__init__.py
@@ -0,0 +1,9 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/11/26 13:40
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+from nonebot import logger
+logger.info("测试插件...")
